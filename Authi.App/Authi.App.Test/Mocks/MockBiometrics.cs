185ab0af3f115c89621eb969b9ce630e3cd29ae2
Boris Gavrikov <cherepets@outlook.com>
2026-09-26 12:39:12 +0200

    Fix tests

---
+++ Authi.App/Authi.App.Test/Mocks/MockBiometrics.cs
@@ -0,0 +1,13 @@
+﻿using Authi.App.Logic.Services;
+using System.Threading.Tasks;
+
+namespace Authi.App.Test.Mocks
+{
+    internal class MockBiometrics : IBiometrics
+    {
+        public Task<bool> VerifyAsync()
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
